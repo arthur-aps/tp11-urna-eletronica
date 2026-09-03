279a6abc03d6f00308396838f57752d80f840369
diogonewerkla <diogonewerkla@gmail.com>
2026-09-03 02:31:34 -0300

    definir candidatos da votação

--- src/main.java
+++ src/main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
         int opcao;
+        int[] votos = new int[6];
+        String[] candidatos = new String[4];
 
         do {
         exibirCabecalho(); // <== METODO COMPARTILHADO (ponto de conflito)
         exibirMenu();
+
         opcao = entrada.nextInt();
         switch (opcao) {
-            case 1: /* funcionalidade do Desenvolvedor A */ break;
+            case 1:
+                votacao(votos, candidatos, entrada);
+            break;
             case 2: /* funcionalidade do Desenvolvedor B */ break;
             case 0: System.out.println("Encerrando..."); break;
             default: System.out.println("Opcao invalida!");
@@ -18,14 +24,23 @@ public class Main {
         } while (opcao != 0);
             entrada.close();
         }
-            public static void exibirCabecalho() {
-            System.out.println("==============================");
-            System.out.println(" NOME DO SISTEMA ");
-            System.out.println("==============================");
-        }
-            public static void exibirMenu() {
-            System.out.println("1 - ...");
-            System.out.println("0 - Sair");
-            System.out.print("Escolha: ");
+
+    public static void exibirCabecalho() {
+        System.out.println("==============================");
+        System.out.println(" NOME DO SISTEMA ");
+        System.out.println("==============================");
+    }
+        
+    public static void exibirMenu() {
+        System.out.println("1 - ...");
+        System.out.println("0 - Sair");
+        System.out.print("Escolha: ");
+    }
+
+    public static void votacao(int[] votos, String[] candidatos, Scanner Entrada){
+        candidatos[0]= "Luis Vinicius Polvo Da Silva";
+        candidatos[1]= "Jairo Messias Bolsanario";
+        candidatos[2]= "Renato Santana";
+        candidatos[3]= "Adalberto Auto-Peças";
     }
 }
